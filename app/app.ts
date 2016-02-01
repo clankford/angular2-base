db4a133f5b967ec809d0eaea5a510555083a049d
Chris Lankford <christopherlankford@gmail.com>
2016-01-31 22:07:48 -0800

    Updated gitingore and cleaned out some old source code

--- app/app.ts
+++ app/app.ts
@@ -1,38 +1,9 @@
 import { Component } from 'angular2/core';
-import { ProjectList } from './components/project-list';
-import { ProjectForm } from './components/project-form';
 
 @Component({
   selector: 'app',
-  directives: [ProjectList, ProjectForm],
   template: `
-    <div class="ui attached stackable menu">
-      <div class="ui container">
-        <a class="item">
-          <i class="home icon"></i> Home
-        </a>
-        <a class="item">
-          <i class="grid layout icon"></i> Browse
-        </a>
-        <a class="item">
-          <i class="mail icon"></i> Messages
-        </a>
-        <div class="ui simple dropdown item">
-          More
-          <i class="dropdown icon"></i>
-          <div class="menu">
-            <a class="item"><i class="edit icon"></i> Edit Profile</a>
-            <a class="item"><i class="globe icon"></i> Choose Language</a>
-            <a class="item"><i class="settings icon"></i> Account Settings</a>
-          </div>
-        </div>
-        <div class="right item">
-      <div class="ui input"><input type="text" placeholder="Search..."></div>
-    </div>
-    </div>
-    </div>
-    <project-form></project-form>
-    <project-list></project-list>
+    Hello World
   `
 })
 export class App {
--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -1,5 +1,4 @@
 import { bootstrap } from 'angular2/platform/browser';
 import { App } from './app';
-import { ProjectService } from './services/project.service';
 
-bootstrap(App, [ProjectService]);
+bootstrap(App);
